fdfaa1091e00909cc7a1c92863dfa8f0efebfc8f
Rosanna <rosanna.fasta@helsinki.fi>
2024-11-08 11:44:01 +0200

    Tehtävä 10

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,21 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        if player_dict.get("nationality") == "FIN":
+            player = Player(player_dict)
+            players.append(player)
+
+    print("Players from FIN:\n")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
---
+++ viikko2/nhl-reader/src/player.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.nationality = dict['nationality']
+    
+    def __str__(self):
+        return (
+            f"{self.name}"
+            f" team {self.team}"
+            f" goals {self.goals}"
+            f" assists {self.assists}"
+        )
